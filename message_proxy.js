0ed506528c64e8eb45a3514ed05fccd741cd9dda
benjaminfrombe <benjamin@plusmore.net>
2026-01-11 22:23:21 +0100

    Fix extension context invalidation error in message_proxy

--- message_proxy.js
+++ message_proxy.js
@@ -1,20 +1,38 @@
 async function processMessage(detail) {
-    return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({
-            type: detail.type,
-            body: detail.body,
-        }, (response) => {
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-            resolve(response);
-        });
-    })
+    try {
+        // This will throw if extension context is invalidated
+        const _ = chrome.runtime.id;
+    } catch (error) {
+        return detail.body;
+    }
+    
+    return new Promise((resolve) => {
+        try {
+            chrome.runtime.sendMessage({
+                type: detail.type,
+                body: detail.body,
+            }, (response) => {
+                if (chrome.runtime.lastError) {
+                    return resolve(detail.body);
+                }
+                resolve(response);
+            });
+        } catch (error) {
+            resolve(detail.body);
+        }
+    });
 }
 
 document.addEventListener('response', async (event) => {
     const { detail } = event;
-    const responseData = await processMessage(detail);
+    let responseData = detail.body;
+
+    try {
+        responseData = await processMessage(detail);
+    } catch (error) {
+        responseData = detail.body;
+    }
+
     const responseEvent = new CustomEvent('responseReceived', {
         detail: detail.requestId.concat(responseData)
     });
